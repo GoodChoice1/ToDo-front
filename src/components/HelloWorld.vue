bf574ff0bfdef20d4dd9de0aac1063ba65eac585
Alexander Fominykh <fominykh.a1809@gmail.com>
2021-10-17 15:29:19 +0300

    Добавил карточки

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <form>
-      <div class="form-field">
-        <label for="name">* Name</label>
-        <input type="text" id="name" required>
+    <div class="card">
+      <h3>
+        Фоминых А.Е.
+      </h3>
+      <div>
+        БСБО-06-19
       </div>
-      <div class ="submit-btn">
-        <input type="submit" value="Submit">
+      <img src = "@/assets/ava.jpg" width="150" height="150" id ="img">
+      <div>
+         Мои навыки:
       </div>
-    </form>
+      <ul>
+        <li>Ем</li>
+        <li>Сплю</li>
+        <li>Держу банан</li>
+      </ul>
+    </div>
+    <div class="card">
+      <div id ="card-text">
+        Форма обратной связи
+      </div>
+      <div class= "form-field">
+        <form>
+          <div id="form-field-name">
+            <input type="text" id="name" placeholder="Ваше имя" required >
+          </div>
+          <div id="form-field-mail">
+            <input type="text" id="mail" placeholder="E-mail" required>
+          </div>
+            <textarea type="text" id="text-area" placeholder="О себе" class="textarea-text"></textarea>
+          <div>
+            <input type="submit" value="Submit" class = "submit-btn">
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,32 +43,64 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 .form-field input  {
   outline: none;
   border: 2px solid black;
   border-radius: 4px;
-  padding: 8px 16px;
+  padding: 4px 8px;
+  margin: 5px;
+  height: 30px;
 }
 
+.card {
+  border: 2px solid black;
+  border-radius: 8px;
+  width: 275px;
+  height: 350px;
+  background: #F29987;
+  box-shadow: 6px 8px 8px gray ;
+  margin: 20px;
+  padding: 10px;
+  line-height: 0.7;
+  display: inline-block;
+  vertical-align: top;
+}
+
+#img {
+  border: 2px solid black;
+  border-radius: 100px;
+  margin: 10px;
+}
+
+.textarea-text{
+  border-radius: 4px;
+  border: 2px solid black;
+  padding: 10px 11px;
+  height: 100px;
+  margin: 5px;
+}
+
+li{
+  margin-bottom: 15px;
+}
+
+#card-text{
+  margin: 15px;
+}
+
+.submit-btn:hover{
+  background: #C0C0C0;
+}
+
+@media screen and (max-width:600px){
+   .button {
+      display: block;
+   }
+}
 </style>
