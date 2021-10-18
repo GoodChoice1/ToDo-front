c65c91f751950e124c1f9756e9c1dfe8983d15e0
Alexander <lol43gg@gmail.com>
2021-10-18 22:34:20 +0300

    Добавил разделение на страницы

---
+++ src/components/FormCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card">
+    <div id ="card-text">
+        Форма обратной связи
+    </div>
+    <div class= "form-field">
+      <form>
+        <div id="form-field-name">
+          <input type="text" id="name" placeholder="Ваше имя" required >
+        </div>
+        <div id="form-field-mail">
+          <input type="text" id="mail" placeholder="E-mail" required>
+        </div>
+          <textarea type="text" id="text-area" placeholder="О себе" class="textarea-text"></textarea>
+        <div>
+          <input type="submit" value="Submit" class = "submit-btn">
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'FormCard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+#card-text{
+  margin: 15px;
+}
+
+.form-field input  {
+  outline: none;
+  border: 2px solid black;
+  border-radius: 4px;
+  padding: 4px 8px;
+  margin: 5px;
+  height: 30px;
+}
+
+.textarea-text{
+  border-radius: 4px;
+  border: 2px solid black;
+  padding: 10px 11px;
+  height: 100px;
+  margin: 5px;
+}
+
+.submit-btn:hover{
+  background: #C0C0C0;
+}
+
+</style>
---
+++ src/components/InfoCardd.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <div class="card">
+      <h3>
+        Фоминых А.Е.
+      </h3>
+      <div>
+        БСБО-06-19
+      </div>
+      <img src = "@/assets/ava.jpg" width="150" height="150" id ="img">
+      <div>
+        Мои навыки:
+      </div>
+      <ul>
+        <li>Ем</li>
+        <li>Сплю</li>
+        <li>Держу банан</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'InfoCard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+#img {
+  border: 2px solid black;
+  border-radius: 100px;
+  margin: 10px;
+}
+
+li{
+  margin-bottom: 15px;
+}
+
+</style>
