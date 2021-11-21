a021dc1e724bfdff955d4c142ce9a23b09ea49df
Alexander Fominykh <fominykh.a1809@gmail.com>
2021-11-21 22:25:50 +0300

    1

--- src/pages/InfoPage.vue
+++ src/pages/InfoPage.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script>
-import InfoCard from "./../components/InfoCardd.vue";
-import FormCard from "./../components/FormCard.vue";
+import InfoCard from "@/components/InfoCardd.vue";
+import FormCard from "@/components/FormCard.vue";
 export default {
   name: "InfoPage",
   components: {
